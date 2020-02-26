d9984397c421badc391e477311ddcdaa8d7b3f14
pedro-aguilarha <61490102+pedro-aguilarha@users.noreply.github.com>
2020-02-26 14:16:24 -0600

    Update preguntar datos.py

--- preguntar datos.py
+++ preguntar datos.py
@@ -2,11 +2,14 @@ import datetime  # se importa un modulo de datatime que se usara en la linea 8
 txt= str(input("inserta una cadena: "))  # a continuacion se piden los datos con los que trabaremos
 entero= int(input("inserta un entero: "))
 dfloat= float(input("inserta un numero decimal: "))
-dtime=str(input("ingresa una fecha yyyy/mm/dd: "))
+dtime=(input("ingresa una fecha yyyy/mm/dd: "))
 # se define una funcion para calcular los tipos de datos
 def calcular(txt,entero,dfloat,dtime):
-    fecha= datetime.datetime(int(dtime[0:4]),int(dtime[5:7]),int(dtime[-2:])) # se ultiliza el modulo
-    resultado= {txt:type(txt),entero:type(entero),dfloat:type(dfloat),"fecha=":fecha}
+    año = dtime[0:4]
+    mes =dtime[5:7]
+    dia= dtime[-2:]
+    fecha= datetime.datetime(int(año),int(mes),int(mes))
+    resultado= {txt:type(txt),entero:type(entero),dfloat:type(dfloat),fecha: type(fecha)}
     return resultado
     # en la linea anterior se guardan el dato intreducido con su perspectivo tipo de dato en un diccionario
 def imprimir(): # se  define una funcion para imprimir
