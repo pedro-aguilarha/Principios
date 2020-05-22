ef8512908004ce5a92b70ab65054032d37c2aa61
pedro-aguilarha <61490102+pedro-aguilarha@users.noreply.github.com>
2020-05-22 18:48:01 -0500

    clase contacto

--- clasepia.py
+++ clasepia.py
@@ -1,13 +1,14 @@
 import datetime
 #import librearia para usar los datos tipo fechas
 class CONTACTO:
-     #se definio la clase
+     #se definio la clase de CONTACTOS
+     #se crea el metodo constructor _init_ para construir objetos cuando se instacie el mismo
     def __init__(self,NickName,Nombre,Correo,Telefono,FechaNacimiento,Gasto,Registro=datetime.datetime.now()):
-        CONTACTO.NickName = NickName
-        CONTACTO.Nombre= Nombre
-        CONTACTO.Correo = Correo
-        CONTACTO.Telefono= Telefono
-        CONTACTO.FechaNacimiento = FechaNacimiento
-        CONTACTO.Gasto= Gasto
-        CONTACTO.Registro= Registro
+        CONTACTO.NickName = NickName      #tipo string                      #se crea un default de registro que se almacenara
+        CONTACTO.Nombre= Nombre           #tipo string                      # un datetime de la hora cuando se instacie el objeto
+        CONTACTO.Correo = Correo          #tipo sitrng
+        CONTACTO.Telefono= Telefono       #tipo string
+        CONTACTO.FechaNacimiento = FechaNacimiento   #tipo datetime
+        CONTACTO.Gasto= Gasto                        #tipo float
+        CONTACTO.Registro= Registro                  #tipo datetime
 
